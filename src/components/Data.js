5b297543a7c4f5018de5427b52013c06f62ee6a9
walidmostafa10 <111182017+walidmostafa10@users.noreply.github.com>
2024-01-31 16:47:05 +0200

    first commit

---
+++ src/components/Data.js
@@ -0,0 +1,44 @@
+export const data = [
+  {
+    id: "1",
+    title: "To-Do",
+    color: "#08243a",
+    class: "ToDo",
+    items: [
+      {
+        content: "Learn JS",
+        id: "1",
+      },
+      {
+        content: "Learn HTML",
+        id: "2",
+      },
+      {
+        content: "Learn CSS",
+        id: "3",
+      },
+      {
+        content: "Learn React",
+        id: "4",
+      },
+      {
+        content: "Learn Bootstrap",
+        id: "5",
+      },
+    ],
+  },
+  {
+    id: "2",
+    title: "In-Progress",
+    color: "#f98538",
+    class: "inprog",
+    items: [],
+  },
+  {
+    id: "3",
+    title: "Done",
+    color: "#c69c6c",
+    class: "finsh",
+    items: [],
+  },
+];
